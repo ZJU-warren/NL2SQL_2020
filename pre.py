bc85033cb3931d6958bbb083d8047aee3faa5962
ZJU-warren <CS_wurui@163.com>
2020-12-08 13:20:49 +0800

    update

--- pre.py
+++ pre.py
@@ -4,12 +4,12 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test_data_ratio', type=float, default=0.3, help='Batch size')
+    parser.add_argument('--test_data_ratio', type=float, default=0.9, help='Batch size')
     args = parser.parse_args()
 
-    # DataGenerator.data_generate([1, 2, 3], cheat_mode=True, test_data_ratio=args.test_data_ratio)
+    DataGenerator.data_generate([1, 2, 3], cheat_mode=True, test_data_ratio=args.test_data_ratio)
     DataGenerator.data_generate([4], cheat_mode=False)
 
-    # DataPreprocessor.preprocess(data_source='Train')
-    # DataPreprocessor.preprocess(data_source='Validation')
+    DataPreprocessor.preprocess(data_source='Train')
+    DataPreprocessor.preprocess(data_source='Validation')
     DataPreprocessor.preprocess(data_source='Test', cheat_mode=False)

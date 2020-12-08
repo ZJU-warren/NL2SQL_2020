50a274023edd8c7c0f013377b7a72ccddceabadc
lizheng3401 <1925130654@qq.com>
2020-12-09 07:45:51 +0800

    up

--- eg.py
+++ eg.py
@@ -1,7 +1,7 @@
 import json
+import numpy as np
 with open('../db/db_schema.json', 'r', encoding='utf-8') as f:
     db = json.load(f)
-print(db)
 x = []
 for it in db:
     table = it["table_name"]
@@ -10,9 +10,12 @@ for it in db:
     for i in range(1, len(col_name)):
         j = int(col_name[i][0].split('_')[-1])
         y += len(table[j]+col_name[i][1])
-        print(table[j]+col_name[i][1])
+    if y>300: print(it['db_name'])
     x.append(y)
 import pandas as pd
 
 d = pd.DataFrame(x)
 print(d.describe())
+x = np.array(x)
+c = (x>400)
+print(np.sum(c))
